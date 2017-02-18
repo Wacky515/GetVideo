c1bd1f32ef39d1c67e7185715bb42609ba4e11e6
wacky515 <wacky515@gmail.com>
2017-02-19 08:57:02 +0900

    Commit before compress

--- setup.py
+++ setup.py
@@ -8,14 +8,6 @@ sys.argv.append("py2exe")
 setup(
         options={"py2exe": {"bundle_files": 3}},
         zipfile=None,
-<<<<<<< HEAD
         # console=[{"script": "recordvideo.py"}]
         console=[{"script": "recordctrl.py"}]
-=======
-        console=[{"script": "recordvideo.py"}]
-<<<<<<< HEAD
-=======
-        # console=[{"script": "recordctrl.py"}]
->>>>>>> 70cb26cd34f68fd51deb10aeacf57f2b9ae11875
->>>>>>> a7fce23a4fc32a82b1a1c70dd6a2f149e7c100e0
 )
